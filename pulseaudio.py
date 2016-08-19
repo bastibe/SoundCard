ef47bfb2fed3461ce031dc9532dc86a9b8e1afd2
Bastian Bechtold <basti@bastibe.de>
2016-08-19 09:46:54 +0200

    refactored for better readability and ability

--- pulseaudio.py
+++ pulseaudio.py
@@ -15,7 +15,7 @@ import numpy
 def all_speakers():
     """A list of all known speakers."""
     with _PulseAudio() as p:
-        return [_Speaker(id=s['id']) for s in p.get_sink_info_list()]
+        return [_Speaker(id=s['id']) for s in p.get_sink_list()]
 
 
 def default_speaker():
@@ -33,14 +33,14 @@ def get_speaker(id):
 
     """
     with _PulseAudio() as p:
-        speakers = p.get_sink_info_list()
+        speakers = p.get_sink_list()
     return _Speaker(id=_match_soundcard(id, speakers)['id'])
 
 
 def all_microphones():
     """A list of all connected microphones."""
     with _PulseAudio() as p:
-        return [Microphone(id=m['id']) for m in p.get_source_info_list()]
+        return [_Microphone(id=m['id']) for m in p.get_source_list()]
 
 
 def default_microphone():
@@ -53,45 +53,36 @@ def default_microphone():
 def get_microphone(id):
     """Get a specific microphone by a variety of means.
 
-    id can be an int index, a pulseaudio id, a substring of the
-    microphone name, or a fuzzy-matched pattern for the microphone
-    name.
+    id can be a pulseaudio id, a substring of the microphone name, or
+    a fuzzy-matched pattern for the microphone name.
 
     """
     with _PulseAudio() as p:
-        microphones = p.get_source_info_list()
-    return Microphone(id=_match_soundcard(id, microphones)['id'])
+        microphones = p.get_source_list()
+    return _Microphone(id=_match_soundcard(id, microphones)['id'])
 
 
 def _match_soundcard(id, soundcards):
     """Find id in a list of soundcards.
 
-    id can be an int index, a pulseaudio id, a substring of the
-    microphone name, or a fuzzy-matched pattern for the microphone
-    name.
+    id can be a pulseaudio id, a substring of the microphone name, or
+    a fuzzy-matched pattern for the microphone name.
 
     """
-    soundcards_by_index = {soundcard['index']: soundcard for soundcard in soundcards}
     soundcards_by_id = {soundcard['id']: soundcard for soundcard in soundcards}
     soundcards_by_name = {soundcard['name']: soundcard for soundcard in soundcards}
-    if isinstance(id, int):
-        if id in soundcards_by_index:
-            return soundcards_by_index[id]
-        else:
-            raise IndexError('no soundcard with id {}'.format(id))
-    elif isinstance(id, str):
-        if id in soundcards_by_id:
-            return soundcards_by_id[id]
-        # try substring match:
-        for name, soundcard in soundcards_by_name.items():
-            if id in name:
-                return soundcard
-        # try fuzzy match:
-        pattern = '.*'.join(id)
-        for name, soundcard in soundcards_by_name.items():
-            if re.match(pattern, name):
-                return soundcard
-        raise IndexError('no soundcard with id {}'.format(id))
+    if id in soundcards_by_id:
+        return soundcards_by_id[id]
+    # try substring match:
+    for name, soundcard in soundcards_by_name.items():
+        if id in name:
+            return soundcard
+    # try fuzzy match:
+    pattern = '.*'.join(id)
+    for name, soundcard in soundcards_by_name.items():
+        if re.match(pattern, name):
+            return soundcard
+    raise IndexError('no soundcard with id {}'.format(id))
 
 
 class _Speaker:
@@ -101,25 +92,74 @@ class _Speaker:
         self._id = id
 
     def __repr__(self):
-        return '<Speaker {}>'.format(self._id)
+        return '<Speaker {} ({} channels)>'.format(self._id, self.channels)
 
-    def player(self, samplerate):
-        return _Player(self._id, samplerate)
+    @property
+    def channels(self):
+        return self._get_info()['channels']
+
+    @property
+    def latency(self):
+        return self._get_info()['latency']
+
+    @property
+    def configured_latency(self):
+        return self._get_info()['configured_latency']
+
+    def _get_info(self):
+        with _PulseAudio() as p:
+            return p.get_sink_info(self._id)
+
+    def player(self, samplerate, blocksize=None):
+        return _Player(self._id, samplerate, self.channels, blocksize=blocksize)
 
     def play(self, data, samplerate):
-        with _Player(self._id, samplerate) as s:
+        with _Player(self._id, samplerate, self.channels) as s:
             s.play(data)
+
+
+class _Microphone:
+    """A soundcard input. Can be used to record audio."""
+
+    def __init__(self, *, id):
+        self._id = id
+
+    def __repr__(self):
+        return '<Microphone {} ({} channels)>'.format(self._id, self.channels)
+
+    @property
+    def channels(self):
+        return self._get_info()['channels']
+
+    @property
+    def latency(self):
+        return self._get_info()['latency']
+
+    @property
+    def configured_latency(self):
+        return self._get_info()['configured_latency']
+
+    def _get_info(self):
+        with _PulseAudio() as p:
+            return p.get_source_info(self._id)
+
+    def recorder(self, samplerate, blocksize=None):
+        return _Recorder(self._id, samplerate, self.channels, blocksize=blocksize)
+
+    def record(self, samplerate, length):
+        with _Recorder(self._id, samplerate, self.channels) as r:
+            return r.record(length)
 
 
 class _Stream:
     """An audio stream."""
 
-    def __init__(self, id, samplerate, type, name='outputstream'):
+    def __init__(self, id, samplerate, channels, blocksize=None, name='outputstream'):
         self._id = id
         self._samplerate = samplerate
         self._name = name
-        self._type = type
-        self.channels = None
+        self._blocksize = blocksize
+        self.channels = channels
 
     def __enter__(self):
         self._pulse = _PulseAudio()
@@ -127,31 +167,26 @@ class _Stream:
         samplespec = ffi.new("pa_sample_spec*")
         samplespec.format = pa.PA_SAMPLE_FLOAT32LE
         samplespec.rate = self._samplerate
-        samplespec.channels = 2
+        samplespec.channels = self.channels
         if not pa.pa_sample_spec_valid(samplespec):
             raise RuntimeException('invalid sample spec')
         self.stream = pa.pa_stream_new(self._pulse.context, self._name.encode(), samplespec, ffi.NULL)
         bufattr = ffi.new("pa_buffer_attr*")
         bufattr.maxlength = 2**32-1 # max buffer length
-        bufattr.fragsize = 2**32-1 # block size
+        bufattr.fragsize = 2**32-1 # recording block size
         bufattr.minreq = 2**32-1 # start requesting more data at this bytes
         bufattr.prebuf = 2**32-1 # start playback after this bytes are available
-        bufattr.tlength = 2**32-1 # buffer length in bytes on server
-        if self._type == 'output':
-            pa.pa_stream_connect_playback(self.stream, self._id.encode(),
-                                          bufattr, pa.PA_STREAM_NOFLAGS, ffi.NULL, ffi.NULL)
-        elif self._type == 'input':
-            pa.pa_stream_connect_record(self.stream, self._id.encode(), bufattr, pa.PA_STREAM_NOFLAGS)
-        while pa.pa_stream_get_state(self.stream) == pa.PA_STREAM_CREATING:
+        bufattr.tlength = self._blocksize*self.channels*4 if self._blocksize else 2**32-1 # buffer length in bytes on server
+        self._connect_stream(bufattr)
+        while pa.pa_stream_get_state(self.stream) not in [pa.PA_STREAM_READY, pa.PA_STREAM_FAILED]:
             time.sleep(0.01)
-        if pa.pa_stream_get_state(self.stream) != pa.PA_STREAM_READY:
+        if pa.pa_stream_get_state(self.stream) == pa.PA_STREAM_FAILED:
             raise RuntimeError('Stream creation failed. Stream is in status {}'.format(pa.pa_stream_get_state(self.stream)))
         channel_map = pa.pa_stream_get_channel_map(self.stream)
         self.channels = int(channel_map.channels)
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        self.channels = None
         operation = pa.pa_stream_drain(self.stream, ffi.NULL, ffi.NULL)
         self._pulse._block_operation(operation)
         pa.pa_stream_unref(self.stream)
@@ -161,8 +196,9 @@ class _Stream:
 
 class _Player(_Stream):
 
-    def __init__(self, id, samplerate, name='outputstream'):
-        super(_Player, self).__init__(id, samplerate, 'output', name)
+    def _connect_stream(self, bufattr):
+        pa.pa_stream_connect_playback(self.stream, self._id.encode(),
+                                      bufattr, pa.PA_STREAM_ADJUST_LATENCY, ffi.NULL, ffi.NULL)
 
     def play(self, data):
         data = numpy.array(data, dtype='float32')
@@ -178,27 +214,10 @@ class _Player(_Stream):
         pa.pa_stream_write(self.stream, bytes, len(bytes), ffi.NULL, 0, pa.PA_SEEK_RELATIVE)
 
 
-class Microphone:
-    """A soundcard input. Can be used to record audio."""
-
-    def __init__(self, *, id):
-        self._id = id
-
-    def __repr__(self):
-        return '<Microphone {}>'.format(self._id)
-
-    def recorder(self, samplerate):
-        return _Recorder(self._id, samplerate)
-
-    def record(self, samplerate, length):
-        with _Recorder(self._id, samplerate) as r:
-            return r.record(length)
-
-
 class _Recorder(_Stream):
 
-    def __init__(self, id, samplerate, name='outputstream'):
-        super(_Recorder, self).__init__(id, samplerate, 'input', name)
+    def _connect_stream(self, bufattr):
+        pa.pa_stream_connect_record(self.stream, self._id.encode(), bufattr, pa.PA_STREAM_ADJUST_LATENCY)
 
     def record(self, num_frames):
         captured_frames = 0
@@ -247,31 +266,51 @@ class _PulseAudio:
             time.sleep(0.001)
         pa.pa_operation_unref(operation)
 
-    def get_source_info_list(self):
+    def get_source_list(self):
         info = []
         @ffi.callback("pa_source_info_cb_t")
         def callback(context, source_info, eol, userdata):
             if not eol:
-                info.append(dict(index=source_info.index,
-                                 name=ffi.string(source_info.description).decode('utf-8'),
-                                 latency=source_info.configured_latency,
+                info.append(dict(name=ffi.string(source_info.description).decode('utf-8'),
                                  id=ffi.string(source_info.name).decode('utf-8')))
         operation = pa.pa_context_get_source_info_list(self.context, callback, ffi.NULL)
         self._block_operation(operation)
         return info
 
-    def get_sink_info_list(self):
+    def get_source_info(self, id):
         info = []
-        @ffi.callback("pa_sink_info_cb_t")
+        @ffi.callback("pa_source_info_cb_t")
         def callback(context, source_info, eol, userdata):
             if not eol:
-                info.append((dict(index=source_info.index,
-                                  name=ffi.string(source_info.description).decode('utf-8'),
-                                  latency=source_info.configured_latency,
-                                  id=ffi.string(source_info.name).decode('utf-8'))))
+                info.append(dict(latency=source_info.latency,
+                                 configured_latency=source_info.configured_latency,
+                                 channels=source_info.sample_spec.channels))
+        operation = pa.pa_context_get_source_info_by_name(self.context, id.encode(), callback, ffi.NULL)
+        self._block_operation(operation)
+        return info[0]
+
+    def get_sink_list(self):
+        info = []
+        @ffi.callback("pa_sink_info_cb_t")
+        def callback(context, sink_info, eol, userdata):
+            if not eol:
+                info.append((dict(name=ffi.string(sink_info.description).decode('utf-8'),
+                                  id=ffi.string(sink_info.name).decode('utf-8'))))
         operation = pa.pa_context_get_sink_info_list(self.context, callback, ffi.NULL)
         self._block_operation(operation)
         return info
+
+    def get_sink_info(self, id):
+        info = []
+        @ffi.callback("pa_sink_info_cb_t")
+        def callback(context, sink_info, eol, userdata):
+            if not eol:
+                info.append(dict(latency=sink_info.latency,
+                                 configured_latency=sink_info.configured_latency,
+                                 channels=sink_info.sample_spec.channels))
+        operation = pa.pa_context_get_sink_info_by_name(self.context, id.encode(), callback, ffi.NULL)
+        self._block_operation(operation)
+        return info[0]
 
     def get_server_info(self):
         info = {}
@@ -310,14 +349,18 @@ if __name__ == '__main__':
     print(all_microphones())
     print('Default Microphone:')
     print(default_microphone())
-    data = numpy.sin(numpy.linspace(0, 2*numpy.pi*100, 44100))
-    default_speaker().play(data, 44100)
-    # with default_speaker().player(44100) as s:
-    #     s.play(data)
-    #     s.play(data)
+    # direct mode:
     data = default_microphone().record(44100, 44100)
-    print(len(data))
     default_speaker().play(data/numpy.max(data), 44100)
-    # import matplotlib.pyplot as plt
-    # plt.plot(numpy.linspace(0, len(data)/44100, len(data)), data)
-    # plt.show()
+    # context manager mode with adjusted blocksize:
+    bl = 2048
+    with default_microphone().recorder(44100, blocksize=bl) as r, default_speaker().player(44100, blocksize=bl) as s:
+        lengths = []
+        length = 0
+        while length < 44100*3:
+            data1 = r.record(1)
+            lengths.append(len(data1))
+            length += len(data1)
+            s.play(data1/numpy.max(data))
+
+    print(numpy.mean(lengths))
--- pulseaudio.py.h
+++ pulseaudio.py.h
@@ -295,8 +295,10 @@ typedef struct pa_source_info {
 
 typedef void (*pa_sink_info_cb_t)(pa_context *c, const pa_sink_info *i, int eol, void *userdata);
 pa_operation* pa_context_get_sink_info_list(pa_context *c, pa_sink_info_cb_t cb, void *userdata);
+pa_operation* pa_context_get_sink_info_by_name(pa_context *c, const char *name, pa_sink_info_cb_t cb, void *userdata);
 typedef void (*pa_source_info_cb_t)(pa_context *c, const pa_source_info *i, int eol, void *userdata);
 pa_operation* pa_context_get_source_info_list(pa_context *c, pa_source_info_cb_t cb, void *userdata);
+pa_operation* pa_context_get_source_info_by_name(pa_context *c, const char *name, pa_source_info_cb_t cb, void *userdata);
 
 typedef struct pa_server_info {
     const char *user_name;
