14513de592c573b0c28a814ec79b381be2474f0e
Bastian <basti@bastibe.de>
2017-05-29 16:29:50 +0200

    fixes setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,13 @@
 from setuptools import setup
 
-if sys.platform == 'darwin':
-    dependencies = ['numpy', 'cffi', 'resampy']
-else:
-    dependencies = ['numpy', 'cffi']
-
 setup(
     name='pythonaudio',
     version='0.1.0',
     description='Play and record audio without resorting to CPython extensions',
+    author='Bastian Bechtold',
+    url='https://github.com/bastibe/Python-Audio',
     license='BSD 3-clause',
     packages=['pythonaudio'],
     package_data={'pythonaudio': ['*.py.h']},
-    install_requires=dependencies
+    install_requires=['numpy', 'cffi']
 )
