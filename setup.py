61bf2ee49c1a9df82d74b9c3e556b196cae87b8f
Bastian <basti@bastibe.de>
2017-10-25 12:41:38 +0200

    increment version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='SoundCard',
-    version='0.1.0',
+    version='0.1.1',
     description='Play and record audio without resorting to CPython extensions',
     author='Bastian Bechtold',
     url='https://github.com/bastibe/SoundCard',
