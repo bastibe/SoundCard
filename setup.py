6450e70f06d8d3eea67463d0e1a3e6cdd167265e
Bastian Bechtold <basti@bastibe.de>
2018-11-29 12:11:29 +0100

    Adds metadata to setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='SoundCard',
-    version='0.2.0',
+    version='0.2.2',
     description='Play and record audio without resorting to CPython extensions',
     author='Bastian Bechtold',
     url='https://github.com/bastibe/SoundCard',
@@ -10,5 +10,18 @@ setup(
     packages=['soundcard'],
     package_data={'soundcard': ['*.py.h']},
     install_requires=['numpy', 'cffi'],
-    python_requires='>=3.6'
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Multimedia :: Sound/Audio :: Capture/Recording',
+        'Topic :: Multimedia :: Sound/Audio :: Players',
+    ],
+    long_description=open('README.rst').read(),
 )
