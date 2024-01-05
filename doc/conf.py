f5918524a96a0d8c913fb65dc8e1cba7facf12d7
Bastian Bechtold <bastibe.dev@mailbox.org>
2024-01-05 09:43:38 +0100

    hopefully unbreaks docs building

--- doc/conf.py
+++ doc/conf.py
@@ -179,15 +179,5 @@ epub_exclude_files = ['search.html']
 
 # -- Extension configuration -------------------------------------------------
 
-from unittest.mock import MagicMock
-
-class CFFIMock(MagicMock):
-    @classmethod
-    def __getattr__(cls, name):
-        return CFFIMock()
-
-    def __eq__(self, other):
-        # so _pa_context_get_state() == PA_CONTEXT_READY
-        return isinstance(other, CFFIMock)
-
-sys.modules.update({'cffi': CFFIMock()})
+import fake_cffi
+sys.modules['cffi'] = sys.modules['fake_cffi']
---
+++ doc/fake_cffi.py
@@ -0,0 +1,6 @@
+class FFI:
+  def cdef(self, _):
+    return NotImplemented
+
+  def dlopen(self, _):
+    return None
