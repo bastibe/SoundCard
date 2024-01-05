cb95ee6cb1710b091ffb113127d98a57b7266309
Bastian Bechtold <bastibe.dev@mailbox.org>
2024-01-05 14:45:34 +0100

    fix documentation build

--- doc/fake_cffi.py
+++ doc/fake_cffi.py
@@ -1,6 +1,23 @@
-class FFI:
-  def cdef(self, _):
-    return NotImplemented
+class PAMeta(type):
+    def __getattr__(self, _):
+        return None
 
-  def dlopen(self, _):
-    return None
+
+class PAStub(metaclass=PAMeta):
+    PA_CONTEXT_READY = None
+
+    def _dummy_function(self, *args, **kwargs):
+        return None
+
+    def __getattr__(self, _):
+        return self._dummy_function
+
+
+class FFI:
+    NULL = None
+
+    def cdef(self, _):
+        return NotImplemented
+
+    def dlopen(self, _):
+        return PAStub()
